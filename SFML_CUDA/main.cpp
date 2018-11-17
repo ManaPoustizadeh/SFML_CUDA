211af0d1d2ee1923b42d3fb6a5e4146470da1751
Mana <mana0pst@gmail.com>
2018-11-17 12:16:37 +0330

    Successfully adds opengl

--- SFML_CUDA/main.cpp
+++ SFML_CUDA/main.cpp
@@ -1,24 +1,84 @@
-#include <SFML/Graphics.hpp>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#define WINDOW_TITLE_PREFIX "Chapter 1"
 
-int main()
+int
+CurrentWidth = 800,
+CurrentHeight = 600,
+WindowHandle = 0;
+
+void Initialize(int, char*[]);
+void InitWindow(int, char*[]);
+void ResizeFunction(int, int);
+void RenderFunction(void);
+
+int main(int argc, char* argv[])
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	Initialize(argc, argv);
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+	glutMainLoop();
 
-		window.clear();
-		window.draw(shape);
-		window.display();
+	exit(EXIT_SUCCESS);
+}
+
+void Initialize(int argc, char* argv[])
+{
+	InitWindow(argc, argv);
+
+	fprintf(
+		stdout,
+		"INFO: OpenGL Version: %s\n",
+		glGetString(GL_VERSION)
+	);
+
+	glClearColor(0.0f, 0.4f, 0.0f, 0.0f);
+}
+
+void InitWindow(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+
+	glutInitContextVersion(4, 0);
+	glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+
+	glutSetOption(
+		GLUT_ACTION_ON_WINDOW_CLOSE,
+		GLUT_ACTION_GLUTMAINLOOP_RETURNS
+	);
+
+	glutInitWindowSize(CurrentWidth, CurrentHeight);
+
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+
+	WindowHandle = glutCreateWindow(WINDOW_TITLE_PREFIX);
+
+	if (WindowHandle < 1) {
+		fprintf(
+			stderr,
+			"ERROR: Could not create a new rendering window.\n"
+		);
+		exit(EXIT_FAILURE);
 	}
 
-	return 0;
+	glutReshapeFunc(ResizeFunction);
+	glutDisplayFunc(RenderFunction);
+}
+
+void ResizeFunction(int Width, int Height)
+{
+	CurrentWidth = Width;
+	CurrentHeight = Height;
+	glViewport(0, 0, CurrentWidth, CurrentHeight);
+}
+
+void RenderFunction(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glutSwapBuffers();
+	glutPostRedisplay();
 }
